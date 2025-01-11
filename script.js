dae924cae799e2e47bf62538c6e1b95519546d0d
Nerayl <souza.allan16@outlook.com>
2025-01-10 22:48:08 -0300

    Add tab system using JS

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ var activeTabName = 'first-tab';
 var activeTabButton;
 
 function activateTab(tabName, button){
-    if (activeTabName != null){
-        var activeTab = document.getElementById(activeTabName);
-        activeTab.style.display = 'none';
-        button.classList.remove('selected');
-    }
+    if (activeTabName != null)
+        document.getElementById(activeTabName).style.display = 'none';
     
-    activeTabName = tabName;
-    
+    if (activeTabButton != null)
+        activeTabButton.classList.remove('selected');
+
     document.getElementById(tabName).style.display = 'block';
     button.classList.add('selected')
+    activeTabName = tabName
     activeTabButton = button;
 }
